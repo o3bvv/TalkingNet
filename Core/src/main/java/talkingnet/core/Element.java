9d80017eb352b49e855d575cbd4a1c9431ff73a5
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-08 11:17:30 +0300

    Added 'Element' abstract class.

---
+++ Core/src/main/java/talkingnet/core/Element.java
@@ -0,0 +1,18 @@
+package talkingnet.core;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public abstract class Element {
+
+    protected String title;
+
+    public Element(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
