6053523938a397ea845430491dd7e86ebeb3ffaa
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-09 15:25:00 +0300

    Modes for UdpPushChannel added.

--- Net/src/main/java/talkingnet/net/udp/channel/UdpPushChannel.java
+++ Net/src/main/java/talkingnet/net/udp/channel/UdpPushChannel.java
@@ -8,12 +8,28 @@ import talkingnet.core.io.channel.PushChannel;
  * @author Alexander Oblovatniy <oblovatniy@gmail.com>
  */
 public class UdpPushChannel extends PushChannel {
+
+    public enum Mode {
+
+        PACKETS, BUFFERS
+    }
+
+    private Mode mode = Mode.PACKETS;
     
     public UdpPushChannel(UdpPushable sink) {
         super(sink);
     }
+
+    public UdpPushChannel(UdpPushable sink, Mode mode) {
+        super(sink);
+        this.mode = mode;
+    }
     
     public void write(DatagramPacket packet) {
-        ((UdpPushable) sink).push_in(packet);
-    }    
+        if (mode.equals(Mode.PACKETS)){
+            ((UdpPushable) sink).push_in(packet);
+        } else {
+            sink.push_in(packet.getData(), packet.getLength());
+        }
+    }
 }
