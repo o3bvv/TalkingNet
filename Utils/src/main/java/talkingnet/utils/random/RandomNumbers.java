2252c946eec5a6c940bbca038599a2ea465e0641
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-25 00:57:25 +0300

    Changed using 'Ceil' to 'Floor' to avoid obtaining numbers equal to range's edge.

--- Utils/src/main/java/talkingnet/utils/random/RandomNumbers.java
+++ Utils/src/main/java/talkingnet/utils/random/RandomNumbers.java
@@ -7,10 +7,10 @@ package talkingnet.utils.random;
 public class RandomNumbers {
 
     public static int getRandom(int edge) {
-        return (int) Math.ceil(Math.random() * edge);
+        return (int) Math.floor(Math.random() * edge);
     }
 
     public static byte getRandomPositiveByte() {
-        return (byte) Math.ceil(Math.random() * Byte.MAX_VALUE);
+        return (byte) Math.floor(Math.random() * Byte.MAX_VALUE);
     }
 }
