b7137f102d50f373eaecfa0d44e48de690c220a7
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-25 10:20:53 +0300

    'getRandomDataOddLength' methd added to random data generator.

--- Utils/src/main/java/talkingnet/utils/random/RandomData.java
+++ Utils/src/main/java/talkingnet/utils/random/RandomData.java
@@ -5,8 +5,20 @@ package talkingnet.utils.random;
  * @author Alexander Oblovatniy <oblovatniy@gmail.com>
  */
 public class RandomData {
-    public static byte[] getRandomData(int lengthEdge){
-        byte[] data = new byte[RandomNumbers.getRandom(lengthEdge)];
+
+    public static byte[] getRandomData(int lengthEdge) {
+        int length = RandomNumbers.getRandom(lengthEdge);
+        return getRandomDataFixedLength(length);
+    }
+
+    public static byte[] getRandomDataOddLength(int lengthEdge) {
+        int length = RandomNumbers.getRandom(lengthEdge);
+        length -= length % 2;
+        return getRandomDataFixedLength(length);
+    }
+    
+    public static byte[] getRandomDataFixedLength(int length) {
+        byte[] data = new byte[length];
         for (int i = 0; i < data.length; i++) {
             data[i] = RandomNumbers.getRandomPositiveByte();
         }
