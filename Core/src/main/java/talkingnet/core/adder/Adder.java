db2c593538f8cb8d1f961fee4edd591ba30c7b5f
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-25 11:29:37 +0300

    'Adder' interface was added.

---
+++ Core/src/main/java/talkingnet/core/adder/Adder.java
@@ -0,0 +1,10 @@
+package talkingnet.core.adder;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public interface Adder {
+    
+    public byte[] getAddedData(byte[][] multiData);
+}
