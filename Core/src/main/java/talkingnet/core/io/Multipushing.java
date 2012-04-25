63687565d7ce91aa9420ea0387c3516f68f8e8af
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-04-25 12:02:27 +0300

    'Multipushing' interface was added.

---
+++ Core/src/main/java/talkingnet/core/io/Multipushing.java
@@ -0,0 +1,10 @@
+package talkingnet.core.io;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public interface Multipushing {
+    
+    void multipush_out(byte[][] data);
+}
