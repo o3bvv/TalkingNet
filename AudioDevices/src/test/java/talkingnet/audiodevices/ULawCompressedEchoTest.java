628ff2e1ca294637852479271a4e7d6aba99b88f
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-05-02 14:39:48 +0300

    ULaw compressed echo-test was added.

---
+++ AudioDevices/src/test/java/talkingnet/audiodevices/ULawCompressedEchoTest.java
@@ -0,0 +1,52 @@
+package talkingnet.audiodevices;
+
+import org.junit.Test;
+import talkingnet.audiodevices.defaults.DefaultAudioSink;
+import talkingnet.audiodevices.defaults.DefaultAudioSource;
+import talkingnet.codecs.ulaw.ULawCompresssor;
+import talkingnet.codecs.ulaw.ULawDecompresssor;
+import talkingnet.core.Pool;
+import talkingnet.utils.audio.DefaultAudioFormat;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public class ULawCompressedEchoTest {
+    
+    private int bufferLengthInMillis = 64;
+    private int bufferLength;
+    
+    private AudioSource source;
+    private ULawCompresssor compresssor;
+    private Pool pool;
+    private ULawDecompresssor decompresssor;
+    private AudioSink sink;
+
+    public ULawCompressedEchoTest() {
+        
+        bufferLength = DefaultAudioFormat.SAMPLING_RATE * DefaultAudioFormat.FRAME_SIZE;
+        bufferLength /= (1000/bufferLengthInMillis);
+        
+        pool = new Pool("pool");
+        
+        decompresssor = new ULawDecompresssor(pool, "decompressor");
+        compresssor = new ULawCompresssor(decompresssor, "compressor");
+        
+        source = new DefaultAudioSource(bufferLength, compresssor, "src");        
+        sink = new DefaultAudioSink(bufferLength, pool, "sink");
+    }
+
+    @Test
+    public void testEcho() throws Exception{
+        sink.open();
+        source.open();
+        sink.start();
+        source.start();
+        
+        Thread.sleep(25000l);
+        
+        source.close();
+        sink.close(); 
+    }
+}
