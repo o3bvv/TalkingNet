691cdc699f685d7a718be64f2dae5c0130a30ae3
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-05-04 22:29:09 +0300

    VAD's current level calculation fixed. Holdtime provided.

--- Core/src/main/java/talkingnet/core/vad/VAD16bit.java
+++ Core/src/main/java/talkingnet/core/vad/VAD16bit.java
@@ -10,36 +10,46 @@ import talkingnet.core.io.Pushing;
  */
 public class VAD16bit extends Element implements Pushable, Pushing {
 
+    private int holdTime = 2500;
+    private int bufferLength;
+    private int holdCounter;
     private float threshold = 0;
     private int maxLevel = Short.MAX_VALUE;
     private float sum;
     private short sample;
     private float currentLevel;
-    
     private Pushable sink;
 
-    public VAD16bit(Pushable sink, String title) {
+    public VAD16bit(int bufferLength, Pushable sink, String title) {
         super(title);
+        this.bufferLength = bufferLength;
         this.sink = sink;
     }
 
     public void push_in(byte[] data, int size) {
         if (isLevelReached(data)) {
-            push_out(data, size);
+            holdCounter = 0;
+        } else {
+            if (holdCounter >= holdTime) {
+                return;
+            }
+            holdCounter += bufferLength;
         }
+
+        push_out(data, size);
     }
 
     private boolean isLevelReached(byte[] data) {
         sum = 0;
-        
-        for (int i = 0; i < data.length; i+=2) {
-            sample = (short) ((data[i+1] << Byte.SIZE) + data[i]);
-            sum += sample / maxLevel;
+
+        for (int i = 0; i < data.length; i += 2) {
+            sample = (short) ((data[i + 1] << Byte.SIZE) + data[i]);
+            sum += ((float) Math.abs(sample)) / maxLevel;
         }
-        
+
         currentLevel = (float) (sum / data.length);
         
-        return currentLevel >= threshold;
+        return (1-currentLevel) >= threshold;
     }
 
     public void push_out(byte[] data, int size) {
@@ -60,5 +70,13 @@ public class VAD16bit extends Element implements Pushable, Pushing {
 
     public void setMaxLevel(int maxLevel) {
         this.maxLevel = maxLevel;
+    }
+
+    public int getHoldTime() {
+        return holdTime;
+    }
+
+    public void setHoldTime(int holdTime) {
+        this.holdTime = holdTime;
     }
 }
