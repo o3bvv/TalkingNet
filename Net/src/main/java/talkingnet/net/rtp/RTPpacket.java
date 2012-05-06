c7ab37323a6c4066cca70c748c7d28e624e3b307
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-05-06 10:45:04 +0300

    RTPpacket: header creation moved to a separate method. #3.

--- Net/src/main/java/talkingnet/net/rtp/RTPpacket.java
+++ Net/src/main/java/talkingnet/net/rtp/RTPpacket.java
@@ -1,44 +1,47 @@
 package talkingnet.net.rtp;
 
 /**
- * @author Alexander Oblovatniy <oblovatniy@gmail.com>
- * <br/>Original:
- * @see http://code.google.com/p/mynpr/source/browse/trunk/mynpr/src/com/webeclubbin/mynpr/RTPpacket.java
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com> <br/>Original:
+ * @see
+ * http://code.google.com/p/mynpr/source/browse/trunk/mynpr/src/com/webeclubbin/mynpr/RTPpacket.java
  */
 public class RTPpacket {
 
-    
     public static int HEADER_SIZE = 12;
     
     public byte[] header;
     public int payload_size;
     public byte[] payload;
     
-    public int Version   = 2;
-    public int Padding   = 0;
+    public int Version = 2;
+    public int Padding = 0;
     public int Extension = 0;
-    public int CC        = 0;
-    public int Marker    = 0;
-    public int Ssrc      = 0;
-    
+    public int CC = 0;
+    public int Marker = 0;
+    public int Ssrc = 0;
     public int PayloadType;
     public int SequenceNumber;
     public int TimeStamp;
-    
-    public RTPpacket(int PType, int Framenb, int Time, byte[] data, int data_length) {
-        //fill changing header fields:
-        SequenceNumber = Framenb;
-        TimeStamp = Time;
-        PayloadType = PType;
 
-        //build the header bistream:
-        //--------------------------
+    /**
+     * RTP packet from raw data
+     */
+    public RTPpacket(int PType, int Framenb, int Time, byte[] data, int data_length) {
+        SequenceNumber = Framenb;
+        TimeStamp      = Time;
+        PayloadType    = PType;
+        payload_size   = data_length;
+
+        createAndFillHeader();
+        
+        payload = new byte[payload_size];
+        
+        System.arraycopy(data, 0, payload, 0, data_length);
+    }
+
+    private void createAndFillHeader() {
         header = new byte[HEADER_SIZE];
 
-        //fill the header array of byte with RTP header fields
-
-        //header[0] = ...
-        // .....
         header[0] = (byte) (Version << 6);
         header[0] = (byte) (header[0] | Padding << 5);
         header[0] = (byte) (header[0] | Extension << 4);
@@ -55,19 +58,11 @@ public class RTPpacket {
         header[9] = (byte) (Ssrc >> 16);
         header[10] = (byte) (Ssrc >> 8);
         header[11] = (byte) (Ssrc & 0xFF);
-
-        //fill the payload bitstream:
-        //--------------------------
-        payload_size = data_length;
-        payload = new byte[data_length];
-        
-        //fill payload array of byte from data (given in parameter of the constructor)
-        System.arraycopy(data, 0, payload, 0, data_length);
     }
-
-    //--------------------------
-    //Constructor of an RTPpacket object from the packet bistream 
-    //--------------------------
+    
+    /**
+     * RTP packet from UDP data
+     */
     public RTPpacket(byte[] packet, int packet_size) {
         //check if total packet size is lower than the header size
         if (packet_size >= HEADER_SIZE) {
@@ -88,7 +83,7 @@ public class RTPpacket {
             TimeStamp = unsigned_int(header[7]) + 256 * unsigned_int(header[6]) + 65536 * unsigned_int(header[5]) + 16777216 * unsigned_int(header[4]);
         }
     }
-    
+
     //--------------------------
     //getpayload: return the payload bistream of the RTPpacket and its size
     //--------------------------
