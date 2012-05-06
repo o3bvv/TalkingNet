63d166b4efa94f8036cc4af77297dc18881dc221
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-05-06 09:55:37 +0300

    RTPpacket: duplicating initialization of attributes, which do not change, moved from constructors to instance variable initializers. #3.

--- Net/src/main/java/talkingnet/net/rtp/RTPpacket.java
+++ Net/src/main/java/talkingnet/net/rtp/RTPpacket.java
@@ -7,37 +7,25 @@ package talkingnet.net.rtp;
  */
 public class RTPpacket {
 
-    //size of the RTP header:
-    static int HEADER_SIZE = 12;
-    //Fields that compose the RTP header
-    public int Version;
-    public int Padding;
-    public int Extension;
-    public int CC;
-    public int Marker;
+    
+    public static int HEADER_SIZE = 12;
+    
+    public byte[] header;
+    public int payload_size;
+    public byte[] payload;
+    
+    public int Version   = 2;
+    public int Padding   = 0;
+    public int Extension = 0;
+    public int CC        = 0;
+    public int Marker    = 0;
+    public int Ssrc      = 0;
+    
     public int PayloadType;
     public int SequenceNumber;
     public int TimeStamp;
-    public int Ssrc;
-    //Bitstream of the RTP header
-    public byte[] header;
-    //size of the RTP payload
-    public int payload_size;
-    //Bitstream of the RTP payload
-    public byte[] payload;
-
-    //--------------------------
-    //Constructor of an RTPpacket object from header fields and payload bitstream
-    //--------------------------
+    
     public RTPpacket(int PType, int Framenb, int Time, byte[] data, int data_length) {
-        //fill by default header fields:
-        Version = 2;
-        Padding = 0;
-        Extension = 0;
-        CC = 0;
-        Marker = 0;
-        Ssrc = 0;
-
         //fill changing header fields:
         SequenceNumber = Framenb;
         TimeStamp = Time;
@@ -81,14 +69,6 @@ public class RTPpacket {
     //Constructor of an RTPpacket object from the packet bistream 
     //--------------------------
     public RTPpacket(byte[] packet, int packet_size) {
-        //fill default fields:
-        Version = 2;
-        Padding = 0;
-        Extension = 0;
-        CC = 0;
-        Marker = 0;
-        Ssrc = 0;
-
         //check if total packet size is lower than the header size
         if (packet_size >= HEADER_SIZE) {
             //get the header bitsream:
@@ -108,7 +88,7 @@ public class RTPpacket {
             TimeStamp = unsigned_int(header[7]) + 256 * unsigned_int(header[6]) + 65536 * unsigned_int(header[5]) + 16777216 * unsigned_int(header[4]);
         }
     }
-
+    
     //--------------------------
     //getpayload: return the payload bistream of the RTPpacket and its size
     //--------------------------
