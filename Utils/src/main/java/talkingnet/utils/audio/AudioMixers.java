f88c6785a45f9466309dd2baab6d4eb630e12e42
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-06-08 13:42:12 +0300

    Methods for getting mixers' names and mixer by name were added to 'AudioMixers'.

--- Utils/src/main/java/talkingnet/utils/audio/AudioMixers.java
+++ Utils/src/main/java/talkingnet/utils/audio/AudioMixers.java
@@ -11,18 +11,34 @@ import javax.sound.sampled.Mixer;
  * @author Alexander Oblovatniy <oblovatniy@gmail.com>
  */
 public class AudioMixers {
- 
+
     public static Collection<Mixer> getAvailableMixers() {
         List<Mixer> result = new ArrayList<Mixer>();
         Mixer.Info[] infos = AudioSystem.getMixerInfo();
         for (int i = 0; i < infos.length; i++) {
-            try {
-                Mixer mixer = AudioSystem.getMixer(infos[i]);
-                result.add(mixer);
-            } catch (Exception ex) {
-                System.out.println(ex);
-            }
+            Mixer mixer = AudioSystem.getMixer(infos[i]);
+            result.add(mixer);
         }
         return result;
+    }
+
+    public static Collection<String> getAvailableMixersNames() {
+        List<String> result = new ArrayList<String>();
+        Mixer.Info[] infos = AudioSystem.getMixerInfo();
+        for (int i = 0; i < infos.length; i++) {
+            result.add(infos[i].getName());
+        }
+        return result;
+    }
+
+    public static Mixer getMixerByNameOrNull(String name) {
+        Mixer.Info[] infos = AudioSystem.getMixerInfo();
+        for (int i = 0; i < infos.length; i++) {
+            Mixer.Info info = infos[i];
+            if (info.getName().equals(name)) {
+                return AudioSystem.getMixer(info);
+            }
+        }
+        return null;
     }
 }
