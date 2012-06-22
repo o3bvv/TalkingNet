e3655efd4c8936bb8df10aa1ac670dfc806cef85
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-06-23 00:46:12 +0300

    Block decryptor was added.

---
+++ Security/src/main/java/ua/cn/stu/cs/talkingnet/security/blockcipher/BlockDecryptor.java
@@ -0,0 +1,37 @@
+package ua.cn.stu.cs.talkingnet.security.blockcipher;
+
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import ua.cn.stu.cs.talkingnet.core.io.Pushable;
+import ua.cn.stu.cs.talkingnet.security.Decryptor;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public class BlockDecryptor extends BufferedBlockCipherWrapper
+        implements Decryptor {
+
+    public BlockDecryptor(
+            BufferedBlockCipher cipher,
+            Pushable sink, String title) {
+        super(cipher, sink, title);
+    }
+
+    public byte[] decrypt(byte[] data, int offset, int length) {
+        return processData(data, offset, length);
+    }
+
+    public void push_in(byte[] data, int size) {
+        byte[] decrypted = decrypt(data, 0, size);
+        push_out(decrypted, decrypted.length);
+    }
+
+    public void push_out(byte[] data, int size) {
+        sink.push_in(data, size);
+    }
+
+    @Override
+    protected boolean isEncrypting() {
+        return false;
+    }
+}
