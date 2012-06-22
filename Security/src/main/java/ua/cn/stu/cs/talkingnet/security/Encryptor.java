529f8670a5d5bcf9dd74a4c7411e724b487ff73e
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-06-22 20:42:27 +0300

    Encryptor and decryptor interfaces were added.

---
+++ Security/src/main/java/ua/cn/stu/cs/talkingnet/security/Encryptor.java
@@ -0,0 +1,13 @@
+package ua.cn.stu.cs.talkingnet.security;
+
+import ua.cn.stu.cs.talkingnet.core.io.Pushable;
+import ua.cn.stu.cs.talkingnet.core.io.Pushing;
+
+/**
+ *
+ * @author Alexander Oblovatniy <oblovatniy@gmail.com>
+ */
+public interface Encryptor extends Pushable, Pushing {
+
+    public byte[] encrypt(byte[] data);
+}
